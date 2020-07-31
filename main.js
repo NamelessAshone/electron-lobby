326ec91fe8b9a85ce6f03d011648a39fed9c2f81
PetTurtle <ryhanillya@gmail.com>
2020-07-31 13:15:06 -0500

    Formated main.js

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const { app, BrowserWindow } = require("electron");
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    webPreferences: {
+	mainWindow = new BrowserWindow({
+		webPreferences: {
 			nodeIntegration: true,
 		},
 	});
