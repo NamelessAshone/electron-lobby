fa5ec3b6d7b0db2880e794c24dbec159d1949672
archangel <deinBestFreund@github.com>
2020-08-03 23:08:21 -0400

    slightly nicer ui

--- lobby.js
+++ lobby.js
@@ -1,5 +1,9 @@
 window.client.on("BATTLEOPENED",(battleid, type, natType, founder, ip, port, maxPlayer, passworded, rank, mapHesh, engineName, engineVersion, map, title, gameName, channel) => {
 	console.log("BATTLE OPENED!!!!!!!!!!!!!!")
-	document.getElementById("gameEntry").innerHTML+="<!--to be inserted by js--><li id = \"gameSubEntry\"><div  class=\"gameInnerSubEntry\" style=\"cursor:pointer; width: 120%; \" type=\"button\" ><p>Battle name:"+title+"</p></br><p>Battle host:"+founder+"</p><p>map:"+map+"</p></div></li><!--to be inserted by js-->"
+	document.getElementById("gameEntry").innerHTML+="<!--to be inserted by js--><li class = \"gameSubEntry\"><div  class=\"gameInnerSubEntry\" style=\"cursor:pointer; width: 60%;background : #2196f3; \" type=\"button\" ><p>Battle name:"+title+"</p></br><p>Battle host:"+founder+"</p><p>map:"+map+"</p></div></li><!--to be inserted by js-->"
+
+
+
+
 });
 
