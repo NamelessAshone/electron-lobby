969ed809e9798b09e731f7e736c4f0e5aa106e97
Lucifer <teresa@eterea.uk>
2020-08-23 00:36:53 -0400

    chatlist functional

--- chat.js
+++ chat.js
@@ -10,11 +10,20 @@ function chatSubmit() {
 	msgQueue[0]=window.username;
 	msgQueue[1] = document.getElementById("chatInput").value;
 	msgPut(msgQueue) //display sent message
-	console.log("fired!")
+	console.log("fired!");
+}
+
+function chatDel(Name) {   
+	console.log("removing"+Name);
+document.getElementById(Name).parentNode.removeChild(document.getElementById(Name));
+
 }
 
 function chatPut(Name) {   //call this function back on joining chat
-document.getElementById("chatList").innerHTML +="<p style=\"background-color: #2196f3;color: white; padding: 5px;\">"+Name+"</p>";
+
+Name;
+document.getElementById("chatList").innerHTML +="<p id=\""+Name+"\"><span style=\"background-color: #2196f3;color: white; padding: 5px;\">"+Name.substring(0, 6)+"</span><span onclick=\"chatDel(&#39;"+Name+"&#39;)\" id=\"chatClose\" class=\"chatClose\" >&#x2715;</span></p>";
+
 }
 chatPut('top10%');
 chatPut('topsmurfs');
