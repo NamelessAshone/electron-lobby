bf11edf78482e4bcf59ef9e2dde30d9ca66d8285
Lucifer <teresa@eterea.uk>
2020-08-24 00:22:10 -0400

    freund functionality

---
+++ freunds.js
@@ -0,0 +1,3 @@
+function frdPut(name,battle){
+	document.getElementById("friendlimitingFrame").innerHTML +="<div class=\"userCard\" id=\"userCard\" style=\"position: relative; height: 17%; width: 19%;margin:0.1%;heght:20%;display: inline-block;padding:1%;\"><span style=\"font-family: JuneBug2\">"+name+"</span><br><span>In "+battle+"<br> For: -\\- hr  -\\-min</span></div>";
+}
