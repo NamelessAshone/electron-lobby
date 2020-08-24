4c19f8129d6e8554afe60f48c05fe8840bc65771
Lucifer <teresa@eterea.uk>
2020-08-24 00:37:54 -0400

    freund fortified

--- freunds.js
+++ freunds.js
@@ -1,3 +1,26 @@
 function frdPut(name,battle){
 	document.getElementById("friendlimitingFrame").innerHTML +="<div class=\"userCard\" id=\"userCard\" style=\"position: relative; height: 17%; width: 19%;margin:0.1%;heght:20%;display: inline-block;padding:1%;\"><span style=\"font-family: JuneBug2\">"+name+"</span><br><span>In "+battle+"<br> For: -\\- hr  -\\-min</span></div>";
 }
+
+frdPut('userA','A\'s gem');
+frdPut('userB','A\'s gem');
+frdPut('userC','A\'s gem');
+frdPut('userD','A\'s gem');
+frdPut('userE','A\'s gem');
+frdPut('userF','A\'s gem');
+frdPut('userG','A\'s gem');
+frdPut('userH','A\'s gem');
+frdPut('userI','A\'s gem');
+frdPut('userJ','A\'s gem');
+frdPut('userK','A\'s gem');
+frdPut('userL','A\'s gem');
+frdPut('userM','A\'s gem');
+frdPut('userN','A\'s gem');
+frdPut('userO','A\'s gem');
+frdPut('userP','A\'s gem');
+frdPut('userQ','A\'s gem');
+frdPut('userR','A\'s gem');
+frdPut('userS','A\'s gem');
+frdPut('userT','A\'s gem');
+frdPut('userU','A\'s gem');
+
